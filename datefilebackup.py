0ca160c70f624d49b8c1eba4f80e5f4c6222da42
Christoph <christoph.schorn@gmail.com>
2016-07-05 20:05:36 +0200

    Add file rename option

--- datefilebackup.py
+++ datefilebackup.py
@@ -4,15 +4,17 @@ import shutil
 from datetime import datetime
 
 
-def date_file_copy(file, path=None):
+def date_file_copy(file, path=None, name=None):
     """
     Copies a file while extending its name by date and time (iso format)
     :param file: file name
     :param path: optional destination path. default: make copy next to the file itself
     """
-    iso_date = datetime.now().strftime('%Y%m%dT%H%M%S')
+    iso_date = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
     file_path, file_name = os.path.split(file)
-    name, ext = os.path.splitext(file_name)
+    file_name, ext = os.path.splitext(file_name)
+    if not name:
+        name = file_name
     new_file = name + '_' + iso_date + ext
 
     dest = path if path else file_path
@@ -25,6 +27,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file', type=str, help="File to backup")
     parser.add_argument('path', nargs='?', type=str, default=None, help="Path to copy the file to")
+    parser.add_argument('name', nargs='?', type=str, default=None, help="New filename")
     args = parser.parse_args()
 
-    date_file_copy(args.file, args.path)
+    date_file_copy(args.file, args.path, args.name)
