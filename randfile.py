fa3132827cb970d248df1e25d0dc5c5074dc953f
Christoph Schorn <christoph.schorn@gmail.com>
2016-12-31 00:41:11 +0100

    Catch error when choosing too many rand files

--- randfile.py
+++ randfile.py
@@ -6,6 +6,7 @@ import glob
 import argparse
 import sys
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('path', nargs='?', type=str, default="**", help="optional path to a specific folder")
@@ -14,8 +15,17 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    files = list(filter(lambda s: os.path.isfile(s), glob.glob(args.path, recursive=args.non_recursive)))
-    rand_files = random.sample(files, k=args.count)
+    # support a 'plain' path like C:/a/b/c
+    # user can also specify his own pattern (e.g. C:/a/**/*.txt)
+    if '*' not in args.path:
+        args.path = os.path.join(args.path, '**')
+
+    rand_files = list(filter(lambda s: os.path.isfile(s), glob.glob(args.path, recursive=args.non_recursive)))
+
+    try:
+        rand_files = random.sample(rand_files, k=args.count)
+    except ValueError:
+        print("Less than {} file(s) found!".format(args.count))
 
     for file in rand_files:
         if sys.platform.startswith('win'):
