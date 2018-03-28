dda1ff1f0e368647177423ac76f67bcb1c405767
Christoph Schorn <christoph.schorn@gmail.com>
2018-03-28 15:46:48 +0200

    Add script to securely enter a variable in the console

---
+++ securevar.cmd
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+set "psCommand=powershell -Command "$pword = read-host 'Enter Password' -AsSecureString ; ^
+    $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+        [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set secret=%%p
+
+(
+    endlocal
+    set secret=%secret%
+)
